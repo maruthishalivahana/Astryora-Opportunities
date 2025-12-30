999dce0a6e36067fcbba30c1074378efbb7d179c
maruthishalivahana <maruthikummari553@gmail.com>
2025-12-30 23:23:28 +0530

    landing page routing

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,13 +1,18 @@
 'use client'
 
 import React from 'react'
+import HowItWorks from '@/components/landingComponents/howItWorks'
+import Header from '@/components/landingComponents/header'
+import AboutAstryora from '@/components/landingComponents/aboutAstryora'
+import HeroSection from '@/components/landingComponents/heroSection'
 
 
 const page = () => {
     return (
         <div>
-            
-           
+            <HeroSection />
+            <AboutAstryora />
+            <HowItWorks />
         </div>
     )
 }
