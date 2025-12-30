912afc923bc4421c329ede014b4d06ea47634d65
Balakumar Petchimuthu <balakumarpetchimuthu77@gmail.com>
2025-12-30 20:15:48 +0530

    Home Page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,17 +1,13 @@
 'use client'
 
 import React from 'react'
-import HeroSection from '@/components/landingComponents/heroSection'
-import AboutAstryora from '@/components/landingComponents/aboutAstryora'
-import HowItWorks from '@/components/landingComponents/howItWorks'
+
 
 const page = () => {
     return (
         <div>
             
-            <HeroSection />
-            <AboutAstryora />
-            <HowItWorks />
+           
         </div>
     )
 }
