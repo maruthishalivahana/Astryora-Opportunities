8e2e0534fa26fd76117693afcc74814f11c3b5ae
maruthishalivahana <maruthikummari553@gmail.com>
2026-01-16 13:54:14 +0530

    fixed

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import HowItWorks from '@/components/landingComponents/howItWorks'
+
 import Header from '@/components/landingComponents/header'
 import AboutAstryora from '@/components/landingComponents/aboutAstryora'
 import HowItWorks from '@/components/landingComponents/howItWorks'
@@ -9,7 +9,6 @@ import TrustAndAudience from '@/components/landingComponents/TrustAndAudience'
 import HeroSection from '@/components/landingComponents/heroSection'
 import FeaturePreview from '@/components/landingComponents/FeaturePreview'
 import WhyAstryoraIsDifferent from '@/components/landingComponents/WhyAstryoraIsDifferent'
-import TrustSafety from '@/components/landingComponents/TrustSafety'
 import WhoThisPlatformIsFor from '@/components/landingComponents/WhoThisPlatformIsFor'
 import Conversion from '@/components/landingComponents/Conversion'
 
@@ -19,14 +18,14 @@ const page = () => {
             <HeroSection />
             <AboutAstryora />
             <HowItWorks />
-            <TrustAndAudience/>
-       </div>
+            <TrustAndAudience />
+
             <FeaturePreview />
             <WhyAstryoraIsDifferent />
-            <TrustSafety />
             <WhoThisPlatformIsFor />
             <Conversion />
         </div>
+
     )
 }
 
